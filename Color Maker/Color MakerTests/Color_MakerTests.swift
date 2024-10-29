eac52f6cfc569ecc31feb5dcf3a1466493f6b301
emilyytsai <112149317+emilyytsai@users.noreply.github.com>
2024-10-29 00:00:16 -0700

    Finished Color Picker App

---
+++ Color Maker/Color MakerTests/Color_MakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Color_MakerTests.swift
+//  Color MakerTests
+//
+//  Created by Emily Tsai on 10/28/24.
+//
+
+import Testing
+@testable import Color_Maker
+
+struct Color_MakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
